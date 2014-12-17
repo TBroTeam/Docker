a50792b5d374cdfdfe9df365cad8ab04358796bd
Markus Ankenbrand <markus@ankenbrand.me>
2014-12-17 21:53:17 +0100

    Fixed linepadding bug. Git archive bug resolved. Attention: was drunk when commiting

--- docker/worker_container/worker_build_installation.sh
+++ docker/worker_container/worker_build_installation.sh
@@ -2,7 +2,7 @@
      cd /home/tbro
 
      # set the correct connection parameter
-     sed -i 's/\${queue_db_host}/'$WORKER_PORT_5432_TCP_ADDR'/' config.php
+     sed -i 's/\${queue_db_host}/'WORKER'/' config.php
      sed -i 's/\${queue_db_name}/'$WORKER_ENV_DB_NAME'/' config.php
      sed -i 's/\${queue_db_port}/'$WORKER_PORT_5432_TCP_PORT'/' config.php
 
