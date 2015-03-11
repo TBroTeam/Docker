4b1cbf09157519c553f34f3f6045aee0caeb6848
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-03-11 18:32:17 +0100

    Fixed detangling issues from emacs
    To allow correct detangling of the Makefiles, accordingly to
    http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-makefile.html
    we have to set the option
    
       (setq org-src-preserve-indentation t)
    
    for orgmode. This breaks our Dockerfiles due to the will be
    indented. To fix the issue, all code blocks have been corrected
    without any indenting. Afterwards all code blocks have been detangled
    again.

--- docker/generic_chado_create_container/generate_db.sh
+++ docker/generic_chado_create_container/generate_db.sh
@@ -1,81 +1,81 @@
 #!/bin/bash
-     export CHADO_DB_NAME=${CHADO_ENV_DB_NAME:-chado}
-     export CHADO_DB_USERNAME=${CHADO_ENV_DB_USER:-tbro}
-     export CHADO_DB_PASSWORD=${CHADO_ENV_DB_PW:-tbro}
-     export CHADO_DB_HOST=${CHADO_PORT_5432_TCP_ADDR:-localhost}
-     export CHADO_DB_PORT=${CHADO_PORT_5432_TCP_PORT:-5432}
+export CHADO_DB_NAME=${CHADO_ENV_DB_NAME:-chado}
+export CHADO_DB_USERNAME=${CHADO_ENV_DB_USER:-tbro}
+export CHADO_DB_PASSWORD=${CHADO_ENV_DB_PW:-tbro}
+export CHADO_DB_HOST=${CHADO_PORT_5432_TCP_ADDR:-localhost}
+export CHADO_DB_PORT=${CHADO_PORT_5432_TCP_PORT:-5432}
 
-     # download chado package
-     date +"[%Y-%m-%d %H:%M:%S] Starting download of chado package..."
-     wget -O /tmp/chado-1.23.tar.gz 'http://sourceforge.net/projects/gmod/files/gmod/chado-1.23/chado-1.23.tar.gz/download'
+# download chado package
+date +"[%Y-%m-%d %H:%M:%S] Starting download of chado package..."
+wget -O /tmp/chado-1.23.tar.gz 'http://sourceforge.net/projects/gmod/files/gmod/chado-1.23/chado-1.23.tar.gz/download'
 
-     cd /tmp/
-     tar xzf /tmp/chado-1.23.tar.gz
+cd /tmp/
+tar xzf /tmp/chado-1.23.tar.gz
 
-     date +"[%Y-%m-%d %H:%M:%S] Finished download of chado package!"
+date +"[%Y-%m-%d %H:%M:%S] Finished download of chado package!"
 
-     date +"[%Y-%m-%d %H:%M:%S] Started preparation of GO 1.2..."
-     cd /tmp
+date +"[%Y-%m-%d %H:%M:%S] Started preparation of GO 1.2..."
+cd /tmp
 
-     wget -O gene_ontology.1_2.obo 'http://www.geneontology.org/ontology/obo_format_1_2/gene_ontology.1_2.obo'
+wget -O gene_ontology.1_2.obo 'http://www.geneontology.org/ontology/obo_format_1_2/gene_ontology.1_2.obo'
 
-     # convertion into xml format this might need the installation of
-     # additional packages and should be moved into the chade database
-     # generation later
-     go2fmt -p obo_text -w xml gene_ontology.1_2.obo | go-apply-xslt oboxml_to_chadoxml - > g_o.1_2.chadoxml
-     date +"[%Y-%m-%d %H:%M:%S] Finished preparation of GO 1.2!"
+# convertion into xml format this might need the installation of
+# additional packages and should be moved into the chade database
+# generation later
+go2fmt -p obo_text -w xml gene_ontology.1_2.obo | go-apply-xslt oboxml_to_chadoxml - > g_o.1_2.chadoxml
+date +"[%Y-%m-%d %H:%M:%S] Finished preparation of GO 1.2!"
 
-     mkdir -p /usr/local/gmod
-     export GMOD_ROOT=/usr/local/gmod
+mkdir -p /usr/local/gmod
+export GMOD_ROOT=/usr/local/gmod
 
-     cd /tmp/chado-1.23/
+cd /tmp/chado-1.23/
 
-     # remove old build.conf if existing
-     if [ -e build.conf ]
-     then
-         rm build.conf
-     fi
+# remove old build.conf if existing
+if [ -e build.conf ]
+then
+    rm build.conf
+fi
 
-     # run the Makefile.PL generator
-     echo "" | perl Makefile.PL
+# run the Makefile.PL generator
+echo "" | perl Makefile.PL
 
-     # the installation name for stag-storenode does not end by an .pl
-     # to circumstand the wrong name I am generating links with the expected names
-     ln -s $(which stag-storenode) $(dirname $(which stag-storenode))/stag-storenode.pl
-     ln -s $(which go2fmt) $(dirname $(which go2fmt))/go2fmt.pl
+# the installation name for stag-storenode does not end by an .pl
+# to circumstand the wrong name I am generating links with the expected names
+ln -s $(which stag-storenode) $(dirname $(which stag-storenode))/stag-storenode.pl
+ln -s $(which go2fmt) $(dirname $(which go2fmt))/go2fmt.pl
 
 
-     # run the make commands
-     make
-     make install
-     make load_schema
-     make prepdb
+# run the make commands
+make
+make install
+make load_schema
+make prepdb
 
-     # importing the function ontology as last ontology
-     make ontologies <<EOF
-     1,2,4,5
-     EOF
+# importing the function ontology as last ontology
+make ontologies <<EOF
+1,2,4,5
+EOF
 
-     # install the prepared GO 1.2
-     date +"[%Y-%m-%d %H:%M:%S] Starting import of own GO 1.2"
-     stag-storenode.pl \
-         -d 'dbi:Pg:dbname='$CHADO_DB_NAME';host='$CHADO_DB_HOST';port='$CHADO_DB_PORT \
-         --user "$CHADO_DB_USERNAME" \
-         --password "$CHADO_DB_PASSWORD" \
-         ../g_o.1_2.chadoxml
+# install the prepared GO 1.2
+date +"[%Y-%m-%d %H:%M:%S] Starting import of own GO 1.2"
+stag-storenode.pl \
+    -d 'dbi:Pg:dbname='$CHADO_DB_NAME';host='$CHADO_DB_HOST';port='$CHADO_DB_PORT \
+    --user "$CHADO_DB_USERNAME" \
+    --password "$CHADO_DB_PASSWORD" \
+    ../g_o.1_2.chadoxml
 
-     if [ $? -ne 0 ]
-     then
-         date +"[%Y-%m-%d %H:%M:%S] Import of GO 1.2 failed, retrying"
-         stag-storenode.pl \
-             -d 'dbi:Pg:dbname='$CHADO_DB_NAME';host='$CHADO_DB_HOST';port='$CHADO_DB_PORT \
-             --user "$CHADO_DB_USERNAME" \
-             --password "$CHADO_DB_PASSWORD" \
-             ../g_o.1_2.chadoxml
+if [ $? -ne 0 ]
+then
+    date +"[%Y-%m-%d %H:%M:%S] Import of GO 1.2 failed, retrying"
+    stag-storenode.pl \
+        -d 'dbi:Pg:dbname='$CHADO_DB_NAME';host='$CHADO_DB_HOST';port='$CHADO_DB_PORT \
+        --user "$CHADO_DB_USERNAME" \
+        --password "$CHADO_DB_PASSWORD" \
+        ../g_o.1_2.chadoxml
 
-     fi
-     date +"[%Y-%m-%d %H:%M:%S] Finished import of own GO 1.2"
+fi
+date +"[%Y-%m-%d %H:%M:%S] Finished import of own GO 1.2"
 
-     # make the optional targets
-     make rm_locks
-     make clean
+# make the optional targets
+make rm_locks
+make clean
--- docker/worker_container/worker_build_installation.sh
+++ docker/worker_container/worker_build_installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-     cd /home/tbro
+cd /home/tbro
 
-     # set the correct connection parameter
-     sed -i 's/\${queue_db_host}/'WORKER'/' config.php
-     sed -i 's/\${queue_db_name}/'$WORKER_ENV_DB_NAME'/' config.php
-     sed -i 's/\${queue_db_port}/'$WORKER_PORT_5432_TCP_PORT'/' config.php
+# set the correct connection parameter
+sed -i 's/\${queue_db_host}/'WORKER'/' config.php
+sed -i 's/\${queue_db_name}/'$WORKER_ENV_DB_NAME'/' config.php
+sed -i 's/\${queue_db_port}/'$WORKER_PORT_5432_TCP_PORT'/' config.php
 
-     # set the correct user parameter
-     sed -i 's/\${queue_db_username}/'$WORKER_ENV_DB_USER'/' config.php
-     sed -i 's/\${queue_db_password}/'$WORKER_ENV_DB_PW'/' config.php
+# set the correct user parameter
+sed -i 's/\${queue_db_username}/'$WORKER_ENV_DB_USER'/' config.php
+sed -i 's/\${queue_db_password}/'$WORKER_ENV_DB_PW'/' config.php
